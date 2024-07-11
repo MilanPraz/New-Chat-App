caf83d4e0c4737f704c12e444229e136edfd9d64
Milanpraz <mln.praz12@gmail.com>
2024-07-11 17:35:12 +0545

    ⚰️ Removed a layout from chatroom

--- src/app/(private)/chatroom/[id]/page.tsx
+++ src/app/(private)/chatroom/[id]/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 import ChatList from "@/components/chatroom/ChatList";
 import SingleChat from "@/components/chatroom/SingleChat";
+import TopBar from "@/components/chatroom/TopBar";
 import { useParams } from "next/navigation";
 import React from "react";
 
 export default function page() {
   return (
-    <div className=" p-4">
-      <ChatList />
+    <div className=" p-4 max-w-3xl text-white">
+      <TopBar />
+      <div>
+        <div className=" p-4">
+          <ChatList />
+        </div>
+      </div>
     </div>
   );
 }
