2122bd80f21b6deadffcf068d03060f4e07bae75
Milanpraz <mln.praz12@gmail.com>
2024-07-11 14:30:11 +0545

    🐛 Improved next config code

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,43 @@
-// next.config.js
+// next.config.js;
 // module.exports = {
 //   images: {
 //     domains: ["chat-app-backend-fwr0.onrender.com"],
 //   },
 // };
+//---------------------
+// const nextConfig = {
+//   images: {
+//     domains: ["chat-app-backend-fwr0.onrender.com"],
+
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "chat-app-backend-fwr0.onrender.com",
+//         pathname: "/**",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["chat-app-backend-fwr0.onrender.com"],
-
-    remotePatterns: [
+  },
+  async headers() {
+    return [
       {
-        protocol: "https",
-        hostname: "chat-app-backend-fwr0.onrender.com",
-        pathname: "/**",
+        source: "/(.*)\\.(png|jpg|webp|svg)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=86400", // Cache for 1 day
+          },
+        ],
       },
-    ],
+    ];
   },
 };
 
